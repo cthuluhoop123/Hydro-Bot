86dd786f34ef7aa1900992ba9b2e5f98d4ddf61f
cthuluhoop123 <dowzhong@gmail.com>
2018-03-29 22:54:41 +1100

    Formatted deleteBulk an quote now works in dm

--- commands/quote.js
+++ commands/quote.js
@@ -3,8 +3,8 @@ exports.run = async function (Discord, client, message, args) {
   let quoteMessage = await message.channel.fetchMessages({ around: quoteID, limit: 1 })
   quoteMessage = quoteMessage.array()[0]
   let embed = new Discord.RichEmbed()
-      .setColor(quoteMessage.member.displayColor)
-      .setAuthor(`${quoteMessage.member.displayName} at ${new Date(quoteMessage.createdTimestamp).toLocaleString()}`, quoteMessage.author.avatarURL)
+      .setColor(quoteMessage.member ? quoteMessage.member.displayColor : 0xaaaaaa)
+      .setAuthor(`${quoteMessage.member ? quoteMessage.member.displayName : quoteMessage.author.username} at ${new Date(quoteMessage.createdTimestamp).toLocaleString()}`, quoteMessage.author.avatarURL)
       .setDescription(quoteMessage.content)
       .setFooter(`Hydro-Bot`)
       .setTimestamp()
